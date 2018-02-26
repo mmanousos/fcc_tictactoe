7f60055de2ff99c71dedefd8ba0a53f4eca73df9
M. Manousos <mmanousos@gmail.com>
2018-02-26 14:30:39 -0800

    logic for computer's first move

--- script.js
+++ script.js
@@ -3,15 +3,7 @@ $(document).ready(function(){
         computerIcon = '', 
         userSquaresArr = [],
         computerSquaresArr = [],
-        boxCount = 0 /*, 
-        cross = $('<img></>')
-            .attr('src', 'cross-small.png')
-            .attr('alt', 'cross icon')
-            .addClass('player-icon'),
-        circle = $('<img></>')
-            .attr('src', 'circle-outline-128.png')
-            .attr('alt', 'circle icon')
-            .addClass('player-icon')*/; 
+        boxCount = 0; 
 
     
   /* hide selection board & display game board */   
@@ -24,18 +16,14 @@ $(document).ready(function(){
     $( '#cross' ).on('click', function() {
         userIcon = 'X';
         computerIcon = 'O';
-        $('#x-turn').removeClass('hidden');
         HideSelect();
     });   
     
     $( '#circle' ).on('click', function() {
         userIcon = 'O';
         computerIcon = 'X';
-        $('#o-turn').removeClass('hidden');
         HideSelect();
     }); 
-    
-
     
   /* user play functionality to display icon */ 
     $('.boxes').on('click', function() {
@@ -47,18 +35,15 @@ $(document).ready(function(){
                 .children('.player-icon-x')
                 .fadeIn(500)
                 .addClass('taken');
-            $('#x-turn').fadeOut(200).addClass('hidden');
-            $('#o-turn').fadeIn(800).removeClass('hidden');
             boxCount++; 
             DeterminePlayer();
             return boxCount;
+            
         } else {
             $(this)
                 .children('.player-icon-o')
                 .fadeIn(500)
-                .addClass('taken');
-            $('#o-turn').addClass('hidden');
-            $('#x-turn').fadeIn(800);
+                .addClass('taken');            
             boxCount++; 
             DeterminePlayer();
             return boxCount; 
@@ -69,61 +54,123 @@ $(document).ready(function(){
     const ComputerPlay = function() {
             console.log('there are ' + boxCount + ' squares filled');
             //computer plays
-            var positionCenter = userSquaresArr.indexOf('box5', 0);
-            var positionCorner1 = userSquaresArr.indexOf('box1', 0);
-            var positionCorner3 = userSquaresArr.indexOf('box3', 0);
-            var positionCorner7 = userSquaresArr.indexOf('box7', 0);
-            var positionCorner9 = userSquaresArr.indexOf('box9', 0);
-            if (positionCenter > -1) {
-                console.log("the user played the center square");
-                computerSquaresArr.push("box1");
+        var positionCenter = userSquaresArr.indexOf('5', 0),
+            positionCorner1 = userSquaresArr.indexOf('1', 0),
+            positionCorner3 = userSquaresArr.indexOf('3', 0),
+            positionCorner7 = userSquaresArr.indexOf('7', 0),
+            positionCorner9 = userSquaresArr.indexOf('9', 0),
+            positionEdge2 = userSquaresArr.indexOf('2', 0),
+            positionEdge4 = userSquaresArr.indexOf('4', 0),
+            positionEdge6 = userSquaresArr.indexOf('6', 0),
+            positionEdge8 = userSquaresArr.indexOf('8', 0);
+        if (boxCount < 2) {
+            if (positionCenter == 0) {
+                computerSquaresArr.push("1");
                 if (computerIcon === 'X') {
-                    $('#box1')
+                    $('#1')
                         .children('.player-icon-x')
                         .delay(800)
                         .fadeIn(500)
                         .addClass('taken');
-                    console.log("computer has played " + computerSquaresArr);
+                    computerSquaresArr.push('1');
+                    console.log("computer has played box " + computerSquaresArr);
                     boxCount++;
-                    $('#o-turn').delay(1300).fadeIn(800);
-                    $('#x-turn').addClass('hidden');
-                    
+
                 } else {
-                    $('#box1')
+                    $('#1')
                         .children('.player-icon-o')
                         .delay(800)
                         .fadeIn(500)
                         .addClass('taken');
-                    console.log("computer has played " + computerSquaresArr);
-                    boxCount++;
-                    $('#o-turn').addClass('hidden');
-                    $('#x-turn').delay(1300).fadeIn(800);
-                }
+                    computerSquaresArr.push('1');
+                    console.log("computer has played box " + computerSquaresArr);
+                    boxCount++;                    
+                }   
             } else if ((positionCorner1 > -1) || (positionCorner3 > -1) || (positionCorner7 > -1) || (positionCorner9 > -1)) {
-                 computerSquaresArr.push("box5");
+                 computerSquaresArr.push("5");
                  if (computerIcon === 'X') {
-                    $('#box5')
+                    $('#5')
                         .children('.player-icon-x')
                         .delay(800)
                         .fadeIn(500)
                         .addClass('taken');
-                     console.log("computer has played " + computerSquaresArr);
-                     boxCount++;
-                     $('#o-turn').addClass('hidden');
-                     $('#x-turn').fadeIn(800);
+                     computerSquaresArr.push('5');
+                     console.log("computer has played box " + computerSquaresArr);
+                     boxCount++;                     
                 } else {
-                    $('#box5')
+                    $('#5')
                         .children('.player-icon-o')
                         .delay(800)
                         .fadeIn(500)
                         .addClass('taken');
-                    console.log("computer has played " + computerSquaresArr);
-                    boxCount++;
-                    $('#x-turn').addClass('hidden');
-                    $('#o-turn').fadeIn(800);    
+                    computerSquaresArr.push('5');
+                    console.log("computer has played box " + computerSquaresArr);
+                    boxCount++;    
                 }      
+            } else if ((positionEdge2 > -1) || (positionEdge4 > -1) || (positionEdge6 > -1) || (positionEdge8 > -1)) {
+                var userSquare = userSquaresArr.length,
+                    lastPlayed = userSquare-1,
+                    userLastPlayed = userSquaresArr[lastPlayed],
+                    nextEdge = '';
+                // if center square isn't taken, play center
+                /* if (!$('#5').hasClass('taken')) {
+                    if (computerIcon === 'X') {
+                        $('#5')
+                            .children('.player-icon-x')
+                            .delay(800)
+                            .fadeIn(500)
+                            .addClass('taken');
+                        computerSquaresArr.push('5');
+                        console.log("computer has played box " + computerSquaresArr);
+                        boxCount++;                     
+                    } else {
+                        $('#5')
+                            .children('.player-icon-o')
+                            .delay(800)
+                            .fadeIn(500)
+                            .addClass('taken');
+                        computerSquaresArr.push('5');
+                        console.log("computer has played box " + computerSquaresArr);
+                        boxCount++;    
+                    }
+                } 
+                // otherwise, play edge opposite (how do I determine this?)
+                  else {
+*/                          
+                    if (userLastPlayed === '2') {
+                        nextEdge = '#8';
+                    } else if (userLastPlayed === '4') {
+                        nextEdge = '#6';
+                    } else if (userLastPlayed === '6') {
+                        nextEdge = '#4';
+                    } else if (userLastPlayed === '8') {
+                        nextEdge = '#2'; 
+                    } 
+                    var calcNextEdge = userLastPlayed;  
+                    if (computerIcon === 'X') {
+                        $(nextEdge)
+                            .children('.player-icon-x')
+                            .delay(800)
+                            .fadeIn(500)
+                            .addClass('taken');
+                        computerSquaresArr.push(nextEdge);
+                        console.log("computer has played box " + computerSquaresArr);
+                        boxCount++;                     
+                    } else {
+                        $(nextEdge)
+                            .children('.player-icon-o')
+                            .delay(800)
+                            .fadeIn(500)
+                            .addClass('taken');
+                        computerSquaresArr.push(nextEdge);
+                        console.log("computer has played box " + computerSquaresArr);
+                        boxCount++;    
+                    }
+                }    
             }
         };
+        
+        
     
     const DeterminePlayer = function() {
         if (boxCount % 2 !== 0) {
@@ -132,14 +179,6 @@ $(document).ready(function(){
             console.log("it's the user's turn.");
         }
     }
-    
-    
-    
-    
-
-    
-    
-    
-    
+  
     
 });
